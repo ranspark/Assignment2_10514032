24a5186bc6b0ddaf33cdd33dad058814c031d28a
ranspark <ranspark2018@gmail.com>
2016-03-17 17:31:52 -0700

    TenTime

---
+++ TenTime.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10514032;
+
+/**
+ *
+ * @author USER
+ */
+public class TenTime {
+    public static void main(String[] args)
+    {
+        for(int i=0;i<9;i++)
+        {
+            System.out.println("Mr. Mitchell is cool.");
+        }
+    }
+}
